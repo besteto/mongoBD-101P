755d3831c9528cd2660b54c62f932c9f02c77b0f
Helen Allien Poe <besteto@gmail.com>
2015-06-09 16:15:46 +0300

    homework 2.2 done

---
+++ HW2/hw2-2.py
@@ -0,0 +1,33 @@
+import pymongo
+import sys
+
+# connnecto to the db on standard port
+connection = pymongo.MongoClient("mongodb://localhost")
+
+db = connection.students                 # attach to db
+collection = db.grades         # specify the colllection
+query = {"type" : "homework"}
+
+try:
+    cursor = collection.find(query).sort([("student_id", pymongo.ASCENDING),("score", pymongo.ASCENDING)])
+
+except Exception as e:
+    print "Error trying to read collection:", type(e), e
+
+first_student_id = -1.0
+
+for every in cursor:
+	if every:
+		if every["student_id"] == first_student_id:
+			print every
+		else:
+			first_student_id = float(every["student_id"])
+			result = collection.delete_one({"student_id": first_student_id, "type" : "homework", "score" : every["score"]})
+
+
+
+#for student_id in range(0,199):
+#	new_cursor = collection.find({"type" : "homework", "student_id" : student_id}).sort("score", pymongo.ASCENDING)
+
+
+
