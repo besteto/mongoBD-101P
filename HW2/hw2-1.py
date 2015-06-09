51675f61cf3a1e131a756f47ec419a259c6c6b99
Helen Allien Poe <besteto@gmail.com>
2015-06-09 12:14:56 +0300

    homework 2.1 done

---
+++ HW2/hw2-1.py
@@ -0,0 +1,20 @@
+import pymongo
+import sys
+
+# connnecto to the db on standard port
+connection = pymongo.MongoClient("mongodb://localhost")
+
+db = connection.students                 # attach to db
+collection = db.grades         # specify the colllection
+query = {"type" : "exam", "score" : { "$gt" : 65}}
+
+try:
+    cursor = collection.find(query, {"student_id" : True}).sort("score", pymongo.ASCENDING).limit(1)
+
+except Exception as e:
+    print "Error trying to read collection:", type(e), e
+
+for every in cursor:
+	print (every)
+
+
